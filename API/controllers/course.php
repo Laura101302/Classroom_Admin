23d440d33c20cdcea515c4a4083df4eaaaacac6d
Laura L. <laurairene2002@gmail.com>
2023-12-16 21:02:16 +0100

    UPDATE - course controller headers

--- API/controllers/course.php
+++ API/controllers/course.php
@@ -1,8 +1,17 @@
 <?php
     header('Content-Type: application/json');
-    header("Access-Control-Allow-Origin:*");
-    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-    header("Access-Control-Allow-Headers: Content-Type");
+    if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+        header("Access-Control-Allow-Origin: http://localhost:4200");
+        header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+        header("Access-Control-Allow-Headers: Content-Type");
+        header("Access-Control-Max-Age: 3600");
+        http_response_code(200);
+        exit();
+    }else{
+        header("Access-Control-Allow-Origin:*");
+        header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+        header("Access-Control-Allow-Headers: Content-Type");
+    }
 
     include_once "../services/course.php";
     require_once "../utils/consts.php";
