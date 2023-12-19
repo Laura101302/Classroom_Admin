378b112d546c0ce6c79ed74d050d8fa9f374c324
Laura L. <laurairene2002@gmail.com>
2023-12-19 19:50:24 +0100

    DELETE - student

--- Web/src/app/app-routing.module.ts
+++ Web/src/app/app-routing.module.ts
@@ -4,8 +4,6 @@ import { TeacherFormComponent } from './teacher/teacher-form/teacher-form.compon
 import { CourseFormComponent } from './course/course-form/course-form.component';
 import { CourseListComponent } from './course/course-list/course-list.component';
 import { TeacherListComponent } from './teacher/teacher-list/teacher-list.component';
-import { StudentListComponent } from './student/student-list/student-list.component';
-import { StudentFormComponent } from './student/student-form/student-form.component';
 import { CenterListComponent } from './center/center-list/center-list.component';
 import { CenterFormComponent } from './center/center-form/center-form.component';
 import { RoleFormComponent } from './role/role-form/role-form.component';
@@ -21,9 +19,6 @@ const routes: Routes = [
   { path: 'teachers', component: TeacherListComponent },
   { path: 'teachers/create-teacher', component: TeacherFormComponent },
   { path: 'teachers/edit-teacher/:dni', component: TeacherFormComponent },
-  { path: 'students', component: StudentListComponent },
-  { path: 'students/create-student', component: StudentFormComponent },
-  { path: 'students/edit-student/:dni', component: StudentFormComponent },
   { path: 'courses', component: CourseListComponent },
   { path: 'courses/create-course', component: CourseFormComponent },
   { path: 'courses/edit-course/:code', component: CourseFormComponent },
--- Web/src/app/app.module.ts
+++ Web/src/app/app.module.ts
@@ -10,8 +10,6 @@ import { CourseFormComponent } from './course/course-form/course-form.component'
 import { CourseListComponent } from './course/course-list/course-list.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { TeacherListComponent } from './teacher/teacher-list/teacher-list.component';
-import { StudentListComponent } from './student/student-list/student-list.component';
-import { StudentFormComponent } from './student/student-form/student-form.component';
 import { CenterListComponent } from './center/center-list/center-list.component';
 import { CenterFormComponent } from './center/center-form/center-form.component';
 import { RoleFormComponent } from './role/role-form/role-form.component';
@@ -25,8 +23,6 @@ import { RoleListComponent } from './role/role-list/role-list.component';
     CourseListComponent,
     NavbarComponent,
     TeacherListComponent,
-    StudentListComponent,
-    StudentFormComponent,
     CenterListComponent,
     CenterFormComponent,
     RoleFormComponent,
