d6d124561a59df413d5771300b876370fd8c14ca
Laura101302 <laurairene2002@gmail.com>
2023-12-22 19:18:40 +0100

    ADD - teacher list table

--- Web/src/app/teacher/teacher-list/teacher-list.component.ts
+++ Web/src/app/teacher/teacher-list/teacher-list.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import { Table } from 'primeng/table';
 import { TeacherService } from 'src/services/teacher.service';
 
 @Component({
@@ -8,10 +9,12 @@ import { TeacherService } from 'src/services/teacher.service';
   styleUrls: ['./teacher-list.component.scss'],
 })
 export class TeacherListComponent {
+  @ViewChild('dt1') dt1: Table | undefined;
   teachers: any[] = [];
   error: boolean = false;
   deleted: boolean = false;
   deletedError: boolean = false;
+  isLoading: boolean = false;
 
   constructor(private teacherService: TeacherService, private router: Router) {}
 
@@ -20,9 +23,17 @@ export class TeacherListComponent {
   }
 
   getAllTeachers() {
+    this.isLoading = true;
+
     this.teacherService.getAllTeachers().subscribe({
-      next: (res: any) => (this.teachers = JSON.parse(res.response)),
-      error: () => (this.error = true),
+      next: (res: any) => {
+        this.teachers = JSON.parse(res.response);
+        this.isLoading = false;
+      },
+      error: () => {
+        this.error = true;
+        this.isLoading = false;
+      },
     });
   }
 
@@ -35,6 +46,8 @@ export class TeacherListComponent {
   }
 
   delete(dni: string) {
+    this.isLoading = true;
+
     this.teacherService.deleteTeacher(dni).subscribe((res) => {
       if (res.code === 200) {
         this.deleted = true;
@@ -46,10 +59,22 @@ export class TeacherListComponent {
       } else {
         this.deleted = false;
         this.deletedError = true;
+        this.isLoading = false;
         setTimeout(() => {
           this.deletedError = false;
         }, 3000);
       }
     });
+  }
+
+  applyFilterGlobal($event: any, stringVal: string) {
+    this.dt1!.filterGlobal(
+      ($event.target as HTMLInputElement).value,
+      stringVal
+    );
+  }
+
+  clearFilter(table: Table) {
+    table.clear();
   }
 }
